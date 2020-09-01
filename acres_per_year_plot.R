fecb305fd8f66452f413528a7decf483f7f41665
Noa Mills <noamills3.14@gmail.com>
2020-09-01 10:55:35 -0700

    Cleaning up, ignoring data files

--- acres_per_year_plot.R
+++
@@ -1,6 +0,0 @@
-firedata <- read.csv('./Data/MTBS_data.csv')
-
-acres_per_year <- vector()
-for(year in 1984:2017)
-  acres_per_year <- c(acres_per_year, sum(firedata[which(firedata$YEAR == year),]$ACRES/1000000))
-plot(1984:2017, acres_per_year, xlab="Year", ylab="Acres Burned in Millions", type="b")
